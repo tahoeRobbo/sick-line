725b3dd5d769a982367fab7c0bd060251e8845c7
tahoeRobbo <robert.r.hallowell@gmail.com>
2019-09-23 17:38:33 -0500

    first commit for sick line! hand rolled to practice setting up new react projects

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/index.js',
+  module: {
+    rules: [
+      { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.(s?)css$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] },
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  } ,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+}
